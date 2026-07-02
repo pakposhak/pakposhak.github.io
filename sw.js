45bd7c290fb0b74267e0121da8ad68c8ddfbcb1e
Danish Wazir <collectionmoors@gmail.com>
2026-07-02 06:03:18 +0600

    build: rebuild after rebase (v187/applychip4)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *  - NEVER touch cross-origin requests (Shopify product fetches, Apps Script order
  *    submission, Formspree). Those must always hit the network untouched.
  */
-const CACHE_VERSION = 'psb-v186';
+const CACHE_VERSION = 'psb-v187';
 const APP_SHELL = [
   './',
   './index.html',
